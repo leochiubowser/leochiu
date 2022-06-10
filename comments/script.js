7f837cb7109fb4342bd34d6ec72ea4f5f8c686b6
leochiubowser <leochiubowser@gmail.com>
2022-06-10 13:31:26 +0800

    add comments components

--- comments/script.js
+++ comments/script.js
@@ -1,21 +1,32 @@
 //讀取Json file 
+var body = document.querySelector("span")
 
-fetch("./data/comments.json")
+fetch("../comments/data/comments.json")
     .then(data => data.json())
     .then(result => {
         Getresult(result);
+        resize(result);
     })
+
+
 
 //將資料傳到函式中處理
 
+
 function Getresult(data) {
+
+
     for (let i = 0; i < data.length; i++) {
         var index = data[i];
         //創建Element 
+
         var title = document.createElement("h1");
         var author = document.createElement("h2");
         var time = document.createElement("p");
         var content = document.createElement("p");
+
+        //刪除Element
+
 
         //從Json file 賦值
         title.textContent = index.title;
@@ -23,10 +34,21 @@ function Getresult(data) {
         time.textContent = index.time.year + "/" + index.time.month + "/" + index.time.date + "  " + index.time.hour + ":" + index.time.minute;
         content.textContent = index.content;
 
-        var body = document.querySelector("body");
-        body.appendChild(title);
-        body.appendChild(author);
-        body.appendChild(time);
-        body.appendChild(content);
+        var div = document.createElement("div");
+        div.appendChild(title);
+        div.appendChild(author);
+        div.appendChild(time);
+        div.appendChild(content);
+        body.appendChild(div)
+
+        var br = document.createElement("br")
+        var hr = document.createElement("hr")
+        body.appendChild(br)
+        body.appendChild(hr)
     }
+}
+
+function resize(result) {
+    var span = document.getElementById("span")
+    span.style.top = 10 + (result.length - 1) * 3 + "cm"
 }
